5aaa38110e89c47b2a2edad18af409cf947f9b2c
Lars Willighagen <lars.willighagen@gmail.com>
2016-08-21 18:09:49 +0200

    Added JSON visualiser for jsonMaker_

--- js/jsonMaker_0.js
+++ js/jsonMaker_0.js
@@ -28,7 +28,7 @@ program
 if ( !process.argv.slice(2).length )
   program.help();
 
-// Making custom logs
+// Set up custom.console
 var custom  = {
       getErrorObject: function () { try { throw Error( '' ) } catch ( e ) { return e; } },
       v_level: {},
@@ -52,7 +52,9 @@ var custom  = {
 	    console.log(
 	      ' '.repeat( 4 - ln.length ) + ln + ': ' +
 	      ( prefix || '[' + name.toUpperCase()[ colour ] + ' '.repeat( 5 - name.length ) ) + '] ' +
-	      Array.prototype.slice.call( arguments ).join( ' ' )
+	      Array.prototype.slice.call( arguments ).map( function ( v ) {
+		return typeof v === 'string' ? v : JSON.stringify( v, null, 2 )
+	      } ).join( ' ' )
 	    )
 	}
       },
@@ -67,14 +69,18 @@ var custom  = {
 
 for ( var i = 0; i < custom.logs.length; i++ ) {
   custom.custom.apply(custom,custom.logs[i]);
-}      
+}
 
+// Set arguments as global variables
 var project = program.project
   , output  = program.output
   , outfile = program.outfile
 
-custom.console.debug( project, output, outfile, program.verbosity )
+custom.console.info( 'Parsing CTree project in folder: ' + project )
+custom.console.info( 'Outputting to folder: ' + output )
+custom.console.info( 'Outputting to file: ' + outfile )
 
+// Validate arguments
 if ( !project ) {
   custom.console.error( 'You must provide a project directory' );
   process.exit( 1 ) }
@@ -83,57 +89,97 @@ if ( !fs.existsSync( project ) ) {
   custom.console.error( 'Project directory does not exist: ' + project );
   process.exit( 1 ); }
 
+// Make output directory if non-existent
 if ( !fs.existsSync( output ) ) {
   custom.console.info( 'Creating output directory: ' + program.output );
   fs.mkdirSync( program.output ); }
 
+// Initiate output data
+var outputData = {
+  articles: [],
+  binomial: {},
+  genus: {}
+}
+
+// Get directories in project folder
+var directories = fs.readdirSync( project )
+
+// For every PMC* directory...
+for ( var dirIndex = 0; dirIndex < directories.length; dirIndex++ ) {
+  
+  var directory = directories[ dirIndex ];
+  
+  if(!/PMC\d+/.test(directory))continue;
+  
+      // ...get JSON...
+  var metadata = JSON.parse( fs.readFileSync(
+	[ project, directory, 'eupmc_result.json' ].join('/'),
+	'utf8' ) )
+      // ...and XML as JSON...
+    , AMIResults = getAMIResults( directory );
+  
+  // ...and append to articles
+  outputData.articles.push( {
+    metadata: metadata,
+    AMIResults: AMIResults
+  } )
+}
+
+
+// Function to turn 'results' folder into JSON
 function getAMIResults ( directory ) {
+  
+  // Get sequencesfiles.xml
   var doc = new xmldoc.XmlDocument( fs.readFileSync(
     [ project, directory, 'sequencesfiles.xml' ].join('/'),
     'utf8' ) )
     , files = doc.children
     , data = {}
   
+  // For every file in sequencesfiles.xml...
   for ( var fileIndex = 0; fileIndex < files.length; fileIndex++ ) {
+    
+    // ...get the file...
     var file = files[fileIndex]
       , fileDoc = new xmldoc.XmlDocument( fs.readFileSync(
       [ project, file.attr.name ].join('/'),
       'utf8' ) )
-      , child = fileDoc.children
+      , children = fileDoc.children.map( function ( v, i ) {
+          return v.attr;
+        } )
     
-    data[ fileDoc.attr.title ] = child.map( function ( v, i ) {
-      return v.attr;
-    } )
+    // ...and return XML as JSON
+    data[ fileDoc.attr.title ] = children
+    
+    // If data contains genera or species...
+    if ( fileDoc.attr.title === 'genus' || fileDoc.attr.title === 'binomial' ) {
+      
+      // ...for every child...
+      for ( var childIndex = 0; childIndex < children.length; childIndex++ ) {
+	
+	var child = children[ childIndex ]
+	  , obj   = outputData[ fileDoc.attr.title ]
+	
+	if ( obj[ child.match ] === undefined )
+	    obj[ child.match ] = [];
+	    
+	    child.pmc = directory;
+	
+	// ...append to resp. arrays as well
+	obj[ child.match ].push(child)
+	
+      }
+      
+    }
   }
   
   return data;
 }
 
-var directories = fs.readdirSync( project )
-  , articles = []
-
-for ( var dirIndex = 0; dirIndex < directories.length; dirIndex++ ) {
-  var directory = directories[ dirIndex ];
-  
-  if(!/PMC\d+/.test(directory))continue;
-  
-  var metadata = JSON.parse( fs.readFileSync(
-	[ project, directory, 'eupmc_result.json' ].join('/'),
-	'utf8' ) )
-    , AMIResults = getAMIResults( directory );
-  
-  articles.push( {
-    metadata: metadata,
-    AMIResults: AMIResults
-  } )
-}
-
 try {
   custom.console.info( 'Saving output to ./' + output + '/' + outfile )
-  fs.writeFileSync( [ output, outfile ].join( '/' ), JSON.stringify( articles ) )
+  fs.writeFileSync( [ output, outfile ].join( '/' ), JSON.stringify( outputData, null, 2 ) )
   custom.console.info( 'Saving output succeeded!' )
 } catch ( e ) {
   custom.console.error( 'Saving output failed!', e.toString() )
 }
-
-process.exit(1);
